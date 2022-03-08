6ad4a1733f575da02afb9c0813ad8c0c90fcb79d
Mayank Jain <mjmayank@gmail.com>
2022-03-08 11:48:25 -0500

    add wallet endpoint and convert sign endpoint to be authenticated

--- app/src/controllers/token_controller.py
+++ app/src/controllers/token_controller.py
@@ -6,6 +6,7 @@ from web3 import Web3
 from app.src.models.models import User
 from app.src.config.database_config import get_db
 from app.src.config.logger_config import LoggerConfig
+from app.src.controllers.auth_controller import get_current_user_id
 from eth_account.messages import encode_defunct
 
 router = APIRouter(prefix="/tokens")
@@ -18,13 +19,21 @@ logger = LoggerConfig(__name__).get()
 #     maskedToken = sha1(token)
 #     user = User.query.filter_by(token=maskedToken)
 
-@router.get("/sign")
+@router.get("/wallet")
+def get_wallet(
+        db: Session = Depends(get_db),
+        user_id: str = Depends(get_current_user_id)
+):
+    user = db.query(User).filter(User.id == user_id).first()
+    return {'data': user.address}
+
+@router.post("/sign")
 def sign_message(
         message: str = "Reddit Year in Review",
-        username: str = "u_123",  # Convert to user_id?
-        db: Session = Depends(get_db)
+        db: Session = Depends(get_db),
+        user_id: str = Depends(get_current_user_id),
 ):
-    user = db.query(User).filter(User.username == username).first()
+    user = db.query(User).filter(User.id == user_id).first()
     ex_msg = bytearray.fromhex(message[2:]).decode()
     message = encode_defunct(text=ex_msg)
     w3 = Web3(Web3.HTTPProvider("https://eth-ropsten.alchemyapi.io/v2/37SaPgF-UEVyGxqZXtDBMKykQt2Ya4Er"))
