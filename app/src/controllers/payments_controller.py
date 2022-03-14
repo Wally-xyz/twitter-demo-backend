a995f6b3ce92da32ce30c06275cb41b6531209c7
John Gold <john@wallylabs.xyz>
2022-03-14 19:29:13 -0400

    payments table and class and service

--- app/src/controllers/payments_controller.py
+++ app/src/controllers/payments_controller.py
@@ -1,21 +1,14 @@
-import os
 
 from fastapi import APIRouter, Depends
-import stripe
-from web3 import Web3
-from web3.types import ABI
 from sqlalchemy.orm import Session
 
-from app.src.config.parameter_store import Properties
+from app.src.services.payment_service import PaymentService
 from app.src.services.auth_service import get_current_user_id
-from app.src.models.models import User
 from app.src.config.database_config import get_db
 from app.src.config.logger_config import LoggerConfig
 
 router = APIRouter(prefix="/payments")
 logger = LoggerConfig(__name__).get()
-
-stripe.api_key = Properties.stripe_api_key
 
 
 @router.post("/checkoutsession")
@@ -23,35 +16,22 @@ def create_checkout_session(
         user_id: str = Depends(get_current_user_id),
         db: Session = Depends(get_db)
 ):
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': 'price_1KdEBBBRQJlh59702ERTzDWh',  # TODO - Calculate on demand? Pull from parameter store?
-                    'quantity': 1,
-                },
-            ],
-            mode='payment',
-            # It seems like this should redirect to a FE url?
-            # Which then pings our backend with additional information...?
-            success_url=Properties.base_domain + '/payments/record?success=True',
-            cancel_url=Properties.base_domain + '/payments/record?success=False',
-        )
-    except Exception as e:
-        return str(e)
-
-    return {"checkout_session": checkout_session.url}
+    redirect_url = PaymentService.create_payment(db=db, user_id=user_id)
+    # The FE needs to redirect this url appropriately with the correct header?
+    return {"checkout_session": redirect_url}
 
 
 # This is hit from the redirect of the PaymentURL thing
-@router.get("/record")
+# Ideally should be a PUT
+@router.get("/{payment_id}/record")
 def create_checkout_session(
         success: bool,
+        payment_id: str,
         user_id: str = Depends(get_current_user_id),
         db: Session = Depends(get_db),
 ):
     # Write success/failure to DB
+    PaymentService.update(db, user_id, payment_id, success)
     # If success, allow the user to continue?
     # Do some stuff
     return {"Redirect result": success}
---
+++ app/src/services/payment_service.py
@@ -0,0 +1,56 @@
+from sqlalchemy.orm import Session
+import stripe
+
+from app.src.config.logger_config import LoggerConfig
+from app.src.config.parameter_store import Properties
+from app.src.models.models import Payment
+from app.src.models.typedefs.PaymentStatus import PaymentStatus
+
+logger = LoggerConfig(__name__).get()
+
+class PaymentService:
+    stripe.api_key = Properties.stripe_api_key
+
+    # Payment
+    @staticmethod
+    def create_payment(db: Session, user_id: str) -> str:
+        payment = Payment(user_id=user_id, status=PaymentStatus.PENDING)
+        db.add(payment)
+        db.commit()
+        db.refresh(payment)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1KdEBBBRQJlh59702ERTzDWh',
+                    # TODO - Calculate on demand? Pull from parameter store?
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            # It seems like this should redirect to a FE url?
+            # Which then pings our backend with additional information...?
+            success_url=Properties.base_domain + f'/payments/{payment.id}/record?success=True',
+            cancel_url=Properties.base_domain + f'/payments/{payment.id}/record?success=False',
+        )
+        # Figure it's good to save this just in case, not sure what other data we get?
+        logger.info(checkout_session)
+        payment.stripe_url = checkout_session.url
+        payment.stripe_id = checkout_session.id
+        payment.amount_cents = checkout_session.amount_total
+        db.commit()
+        return checkout_session.url
+
+    # Update Payment
+    # Payment
+    @staticmethod
+    def update(db: Session, user_id: str, payment_id: str, success: bool) -> Payment:
+        payment = db.query(Payment).filter(Payment.id == payment_id).first()
+        if payment.user_id != user_id:
+            raise Exception("Unauthorized update")
+        if success:
+            payment.status = PaymentStatus.SUCCESS
+        else:
+            payment.status = PaymentStatus.FAILURE
+        db.commit()
+        return payment
