34a10543c78c969410bbe0edc01227069511e911
John Gold <john@wallylabs.xyz>
2022-03-16 09:52:46 -0400

    upload

--- app/src/clients/pinata_client.py
+++ app/src/clients/pinata_client.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 BASE_URL = "https://api.pinata.cloud"
 PIN_URL = f"{BASE_URL}/pinning/pinFileToIPFS"
 
+PIN_JSON_URL = f"{BASE_URL}/pinning/pinJSONToIPFS"
 
 @dataclass
 class PinFileResponse:
--- app/src/services/media_service.py
+++ app/src/services/media_service.py
@@ -9,7 +9,7 @@ import requests
 from fastapi import UploadFile
 from sqlalchemy.orm import Session
 
-from app.src.clients.pinata_client import PIN_URL, PinFileResponse
+from app.src.clients.pinata_client import PIN_URL, PinFileResponse, PIN_JSON_URL
 from app.src.config.logger_config import LoggerConfig
 from app.src.config.parameter_store import Properties
 from app.src.models.models import User, Media
@@ -51,27 +51,42 @@ class MediaService:
                          }}
 
         # TODO - IPFS Metadata using name/description fields
-        result = requests.post(url=PIN_URL,
-                               files={"file": contents},
-                               data={
-                                   "pinataOptions": '{"cidVersion":0}',
-                                   "pinataMetadata": json.dumps(ipfs_metadata)
-                               },
-                               headers=headers)
+        media_result = requests.post(url=PIN_URL,
+                                     files={"file": contents},
+                                     data={
+                                         "pinataOptions": '{"cidVersion":0}',
+                                         "pinataMetadata": json.dumps(ipfs_metadata)
+                                     },
+                                     headers=headers)
 
-        logger.info(result.json())
+        ipfs_hash = media_result.json()["IpfsHash"]
+
+        json_file = {'name': data.name,
+                     'description': data.description,
+                     'image': f'https://gateway.pinata.cloud/ipfs/{ipfs_hash}'
+                     }
+        ipfs_metadata["name"] = "json-" + ipfs_metadata["name"]
+
+        json_result = requests.post(url=PIN_JSON_URL,
+                                    json={
+                                        "pinataOptions": {"cidVersion": 0},
+                                        "pinataMetadata": ipfs_metadata,
+                                        "pinataContent": json_file,
+                                    },
+                                    headers=headers)
+
+        logger.info(media_result.json())
 
         # Also put the file to S3
         s3_key = str(uuid.uuid4())
         boto3.client("s3").upload_fileobj(io.BytesIO(contents), Properties.media_bucket, s3_key)
 
-        ipfs_hash = result.json()["IpfsHash"]
-        pin_size = result.json()["PinSize"]
+        json_ipfs_hash = json_result.json()["IpfsHash"]
 
         # TODO - Better unwinding of the response object
         # response = PinFileResponse(**result.json())
 
-        media_object = Media(ipfs_hash=ipfs_hash, pin_size=pin_size, filename=filename, key=s3_key,
+        media_object = Media(ipfs_hash=json_ipfs_hash, filename=filename, key=s3_key,
                              user_id=user.id, name=data.name, description=data.description)
         db.add(media_object)
         db.commit()
