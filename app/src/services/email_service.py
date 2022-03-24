a9753096363b398cbc5d91120d48a4a9b3647b7c
Mayank Jain <mjmayank@gmail.com>
2022-03-24 14:04:26 -0400

    Updated mined email to use new template

--- app/src/services/email_service.py
+++ app/src/services/email_service.py
@@ -10,7 +10,7 @@ class EmailService:
     @staticmethod
     def send_verification_code(user: User, code: str):
         message = Mail(
-            from_email='hello@wallylabs.xyz',
+            from_email=('hello@wallylabs.xyz', 'Wally Labs'),
             to_emails=user.email)
         message.reply_to = 'mayank@wallylabs.xyz'
         message.template_id = 'd-ec91b40826a54c2d9d324da8341461fb'
@@ -25,10 +25,15 @@ class EmailService:
     @staticmethod
     def send_mined_email(user: User, tx_hash: str):
         message = Mail(
-            from_email='hello@wallylabs.xyz',
-            to_emails=user.email,
-            subject='Your NFT has been Mined',
-            html_content=f'Congratulations, your NFT has been minted. View it on etherscan: '
-                         f'https://etherscan.io/tx/{tx_hash}')
+            from_email=('hello@wallylabs.xyz', 'Wally Labs'),
+            to_emails=user.email)
+        message.reply_to = 'mayank@wallylabs.xyz'
+        message.template_id = 'd-b52b1d6cefff4b41a0850eabdb82ef5e'
+        message.dynamic_template_data = {
+            'etherscan_url': f'https://etherscan.io/tx/{tx_hash}',
+            'img_url': 'https://www.wallylabs.xyz/logo.png',
+            'opensea_url': 'https://testnets.opensea.io/collection/wally',
+            'url': Properties.frontend_url + f'?step=5'
+        }
         sg = SendGridAPIClient(Properties.sendgrid_api_key)
         sg.send(message)
