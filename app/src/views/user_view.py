4f4eb7e53093f54670b396e7fe4bc47bddba1b8b
Mayank Jain <mjmayank@gmail.com>
2022-04-06 12:59:28 -0400

    Update flow to use endpoints with proper parameters

--- app/src/views/user_view.py
+++ app/src/views/user_view.py
@@ -8,11 +8,9 @@ from app.src.models.models import User
 class UserView:
     id: str
     email: str
-    address: Optional[str]
     twitter_handle: Optional[str]
 
     def __init__(self, user: User):
         self.id = user.id
         self.email = user.email
-        self.address = user.address
         self.twitter_handle = user.twitter_handle
